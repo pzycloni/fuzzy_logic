e35a3806853c2bae8cee00e255f701f87f61cccb
Alexey Poltarak <gukklucky@gmail.com>
2017-04-15 02:55:57 +0500

    Изменен подход к написанию приложения.

--- source.py
+++ source.py
@@ -1,154 +1,133 @@
 import math
 
-def get_points(start, end, step, incline):
-	points = list()
+# координаты точки
+class Coordinate:
 
-	if incline == -1:
-		for point in range(start, end, step):
-			points.append((point - start) / (end - start))
+	def __init__(self, x, y):
+		self.x, self.y = x, y
 
-	if incline == 1:
-		for point in range(start, end, step):
-			points.append((end - point) / (end - start))
+# касательная
+class Tangent:
 
-	if incline == 0:
-		for point in range(start, end, step):
-			points.append(1)
+	def __init__(self, incline, start, end):
+		self.incline = incline
+		self.start = start
+		self.end = end
+		# все точки касательной
+		self.coordinates = self.get_coordinates()
 
-	return points
+	# точки касательной
+	def get_coordinates(self, step = 1):
+		# все точки
+		points = list()
+		# отрицательный наклон касательной
+		if self.incline == -1:
+			for point in range(self.start, self.end, step):
+				coordinate = Coordinate(point, (point - self.start) / (self.end - self.start))
+				points.append(coordinate)
+		# положительный наклон касательной
+		if self.incline == 1:
+			for point in range(self.start, self.end, step):
+				coordinate = Coordinate(point, (self.end - point) / (self.end - self.start))
+				points.append(coordinate)
+		# касательная параллельна оси Х
+		if self.incline == 0:
+			for point in range(self.start, self.end, step):
+				coordinate = Coordinate(point, 1)
+				points.append(coordinate)
 
-# получаем mu
-def sensor(temperature):
+		return points
 
-	result = dict()
+# фигура(множество), состоящая из касательных
+class Figure:
 
-	if temperature <= 30:
-		result['холодная'] = 1 if temperature <= 10 else (30 - temperature) / (30 - 10)
-
-	if temperature >= 20 and temperature <= 50:
-		result['прохладная'] = (temperature - 20) / (35 - 20) if temperature <= 35 else (50 - temperature) / (50 - 35)
-
-	if temperature >= 40 and temperature <= 60:
-		result['теплая'] = (temperature - 40) / (50 - 40) if temperature <= 50 else (60 - temperature) / (60 - 50)
-
-	if temperature >= 50 and temperature <= 70:
-		result['не очень горячая'] = (temperature - 50) / (60 - 50) if temperature <= 60 else (70 - temperature) / (70 - 60)
-
-	if temperature >= 60:
-		result['горячая'] = (temperature - 60) / (70 - 60) if temperature <= 70 else 1
-
-	return result
-
-# правила нечетких продукций(вывод)
-def controller(water):
-
-	action = list()
-
-	if 'горячая' in water:
-		action.append("намного уменьшить")
-
-	if 'не очень горячая' in water:
-		action.append("уменьшить")
-
-	if 'теплая' in water:
-		action.append("оставить")
-
-	if 'прохладная' in water:
-		action.append("увеличить")
-
-	if 'холодная' in water:
-		action.append("намного увеличить")
-
-	return action
+	def __init__(self, name, tangents = []):
+		self.tangents = tangents
+		self.name = name
 
 
-def max_disjunction(set_mu, max_mu):
+# действия с нечеткими множествами
+class Machine:
 
-	set_mu = [mu for mu in set_mu if mu <= max_mu]
+	def __init__(self):
+		self.figures = list()
+	# добавление множества
+	def add_figure(name, tangents = []):
+		# новое множество
+		figure = Figure(name, tangents)
+		# добавляем новое множество
+		self.figures.append(figure)
 
-	return max(set_mu)
+	# по температуре воды определяет какая вода
+	def sensor_water(self, temperature):
 
-# входные параметры(лингвистическая переменная): 
-# температура воды
-def start(temperature):
-	# получаем mu
-	set_temprature = sensor(temperature)
-	# получаем нужные правила нечетких продукций
-	# используются в текущем процессе нечеткого вывода
-	action = controller([key for key in set_temprature.keys()])
+		result = dict()
 
-	# отсекаем 0
-	# блок агрегирования 
-	# (вычисление степени истинности условий по каждому правилу продукций)
-	set_temprature = [mu for mu in set_temprature.values() if mu > 0]
+		if temperature <= 30:
+			result['холодная'] = 1 if temperature <= 10 else (30 - temperature) / (30 - 10)
 
-	# блок активизации (вычисление степени истинности каждого из подзаключений правил продукции и построение функции принадлежности каждого из подзаключений для рассматриваемых выходных лингвистических переменных)
-	mu = min(set_temprature)
-	
-	set_mu = list()
-	borders = list()
+		if temperature >= 20 and temperature <= 50:
+			result['прохладная'] = (temperature - 20) / (35 - 20) if temperature <= 35 else (50 - temperature) / (50 - 35)
 
-	# получаем точки нужных правил продукций
-	if 'намного уменьшить' in action:
-		set_mu.extend(get_points(30, 40, 2, 0))
-		set_mu.extend(get_points(20, 30, 2, 1))
-		# запоминаем граицы продукций
-		borders.extend([20, 40])
+		if temperature >= 40 and temperature <= 60:
+			result['теплая'] = (temperature - 40) / (50 - 40) if temperature <= 50 else (60 - temperature) / (60 - 50)
 
-	if 'уменьшить' in action:
-		set_mu.extend(get_points(5, 15, 2, 1))
-		set_mu.extend(get_points(15, 25, 2, -1))
-		# запоминаем граицы продукций
-		borders.extend([5, 25])
+		if temperature >= 50 and temperature <= 70:
+			result['не очень горячая'] = (temperature - 50) / (60 - 50) if temperature <= 60 else (70 - temperature) / (70 - 60)
 
-	if 'оставить' in action:
-		set_mu.extend(get_points(-15, 0, 2, 1))
-		set_mu.extend(get_points(0, 15, 2, -1))
-		# запоминаем граицы продукций
-		borders.extend([-15, 15])
+		if temperature >= 60:
+			result['горячая'] = (temperature - 60) / (70 - 60) if temperature <= 70 else 1
 
-	if 'увеличить' in action:
-		set_mu.extend(get_points(-25, -15, 2, 1))
-		set_mu.extend(get_points(-15, -5, 2, 0))
-		# запоминаем граицы продукций
-		borders.extend([-25, -5])
+		return result
 
-	if 'намного увеличить' in action:
-		set_mu.extend(get_points(-30, -20, 2, -1))
-		set_mu.extend(get_points(-40, -30, 2, 0))
-		# запоминаем граицы продукций
-		borders.extend([-40, -20])
+	# по весу белья определяет количество белья
+	def sensor_weight(self, weight):
 
-	# блок аккумуляции 
-	# (вычисление функции принадлежности для каждой из лингвистических переменных)
-	mu = max_disjunction(set_mu, mu)
-	# выбираем граицы продукций
-	border_min = min(borders)
-	border_max = max(borders)
+		result = dict()
 
-	# блок дефаззификации (нахождение обычного (не нечеткого)
-	# значения выходной переменной(ых))
-	
-	# находим точку
-	# метод взвешенной точки
-	x0 = (1/6) * border_min + (4/6) * (mu + mu)/2 + (1/6) * border_max
+		if weight <= 1:
+			result['мало'] = 1 if weight <= 1 else (1 - weight) / (1 - 0)
 
-	return -1 * math.ceil(x0)
+		if weight >= 0.5 and weight <= 3:
+			result['немного'] = (weight - 0.5) / (1.25 - 0.5) if weight <= 1.25 else (3 - weight) / (3 - 1.25)
+
+		if weight >= 2.5:
+			result['много'] = (weight - 2.5) / (4 - 2.5) if weight <= 4 else 1
+
+		return result
+
+	#
+	def controller_water(self, feature):
+		actions = list()
+
+		if 'холодная' in feature:
+			actions.append('расход жидкости большой')
+		if 'прохладная' in feature:
+			actions.append('расход жидкости небольшой')
+		if 'теплая' in feature or 'не очень горячая' in feature:
+			actions.append('расход жидкости средний')
+		if 'горячая' in feature:
+			actions.append('расход жидкости большой')
+
+		return actions
 
 
-# используем алгоритм Мамдани
-	
-# в качестве входной 
-# лингвистической переменной следует использовать температуру воды
+	# запуск стиральной машинки
+	def start(self, temperature, weight):
+		# получаем все mu температуры воды
+		feature_water = self.sensor_water(temperature)
+		# получаем все mu веса белья
+		feature_weight = self.sensor_weight(weight)
 
-# в качестве выходной лингвистической переменной 
-# будем использовать угол поворота регулятора горячей воды
+
+
+
 if __name__ == '__main__':
-	# вода
-	result = start(55)
+	washine = Machine()
 
-	if result >= 0:
-		print("Повернуть регулятор температуры на {0}* вправо".format(result))
-	else:
-		print("Повернуть регулятор температуры на {0}* влево".format(-1*result))
+	tangents = [Tangent(-1, 15, 25), 
+				Tangent(1, 5, 15)]
 
+	figure = Figure('уменьшить', tangents)
+
+	#(figure.tangents[0].coordinates)
